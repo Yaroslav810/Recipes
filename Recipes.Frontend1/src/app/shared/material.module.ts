f294f46a264e7f0e84dcfad21e043f75caae5f65
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2021-05-01 11:00:37 +0300

    R-2: Deleted unnecessary files

--- Recipes.Frontend1/src/app/shared/material.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { MatListModule } from '@angular/material/list';
-
-@NgModule({
-  exports: [
-    MatListModule
-  ]
-})
-export class MaterialModule { }
